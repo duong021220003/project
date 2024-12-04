66d2af56dd33ee243e8945e9fcd095e51c1f2438
Nguyen Quy Duong <nguyenquyduong02122003@gmail.com>
2024-12-04 17:48:12 +0700

    test env.py

--- test env.py
+++ test env.py
@@ -1,10 +1,10 @@
 import numpy as np
 import gym
-
+import pickle
 env = gym.make('gym_robot_arm:robot-arm-v0')
 min_theta = np.radians(0)
 max_theta = np.radians(90)
-q_table_size = [10, 10]
+q_table_size = [2,2]
 q_table_segment_size = (max_theta - min_theta) / q_table_size
 
 
@@ -14,15 +14,21 @@ def convert_state(real_state):
     return tuple(q_state)
 
 
-q = np.random.uniform(size=(q_table_size + [env.action_space.n]))
-episodes = 1000
-max_steps = 200
+training = True
+if (training):
+    q = np.random.uniform(size=(q_table_size + [env.action_space.n]))
+else:
+    f = open('gym_robot_arm:robot-arm-v0.pkl', 'rb')
+    q = pickle.load(f)
+    f.close()
+
+episodes = 100
+max_steps = 100
 alpha = 0.9
 gamma = 0.9
-training = True
 
-show = 10
-epsilon = 1
+
+epsilon =200
 epsilon_decay_rate = 2 / episodes
 rng = np.random.default_rng()
 rewards_per_episode = np.zeros(episodes)
@@ -31,15 +37,15 @@ action = env.action_space.sample()
 max_reward = -999
 max_action = []
 observation = env.reset()
-for i in range(episodes):
-    theta = [observation[2], observation[3]]
+print("Target là ",observation[0],observation[1])
+for a in range(episodes):
+    theta = (observation[2], observation[3])
     state = convert_state(theta)
     ep_reward = 0
     action_list = []
     env.render()
-    for _ in range(max_steps):
 
-
+    for b in range(max_steps):
         if training:
             if rng.random() < epsilon:
                 action = env.action_space.sample()
@@ -53,14 +59,25 @@ for i in range(episodes):
         ep_reward += reward
         distance_error = info['distance_error']
         new_state = convert_state((new_theta[2], new_theta[3]))
-        q[state][action] += alpha * (
-                reward + gamma * np.max(q[new_state]) - q[state][action]
-        )
+
+        current_q=q[state+(action,)]
+
+        q += alpha * ( reward + gamma * np.max(q[new_state]) - q[state][action])
+
+        q[state+(action,)]=current_q
         state = new_state
         ep_reward += reward
-        if done:
-            if distance_error < 0.1:
-             print("Đã chạm tới đích,")
-    env.close()
 
-
+        #print("Q Table là",q)
+        if distance_error < epsilon:
+            print("giá trị theta là ",theta)
+            print("Đã chạm tới đích tại eps={},steps={}".format(a,b))
+            print("giá trị distance: ",distance_error)
+            done=True
+            print ("giá trị theta={},action={}".format(new_theta,action))
+        # if distance_error>epsilon:
+        #     print("Chưa tới")
+        if training:
+            f = open('gym_robot_arm:robot-arm-v0.pkl', 'wb')
+            pickle.dump(q, f)
+            f.close()
